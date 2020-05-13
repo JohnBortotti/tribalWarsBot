d3c9ec09782b55b119f5161a3aedc34ba3e25902
John Bortotti <joao_bortotti@hotmail.com>
2020-05-13 02:58:14 -0300

    refactoring and starting at updates

--- main.js
+++ main.js
@@ -15,61 +15,76 @@ _______   _ _           _   ____        _
 console.log('\nCreated by: John Bortotti\n')
 const username = prompt('username: ');
 const password = prompt('password: ');
+const delayTime = prompt('Delay for actions (in minutes): ')
 
-const main = async (username, password) => {
+function sleep(milliseconds) { 
+  let timeStart = new Date().getTime(); 
+  while (true) { 
+      let elapsedTime = new Date().getTime() - timeStart; 
+      if (elapsedTime > milliseconds) { 
+          break; 
+      } 
+  } 
+} 
 
-  const browser = await puppeteer.launch({ headless: true });
+
+const launchAndGetPage = async() => {
+
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://www.tribalwars.net/en-dk/');
 
+  return page;
+}
 
-  // Login //
+const login = async(instance, usr, password) => {
 
-  // Username Input //
-  const usernameInput = await page.$('#user');
+  const usernameInput = await instance.$('#user');
   await usernameInput.click();
-  await page.keyboard.type(`${username}`);
+  await instance.keyboard.type(`${usr}`);
 
-  // Password Input //
-  const passwordInput = await page.$('#password');
+  const passwordInput = await instance.$('#password');
   await passwordInput.click();
-  await page.keyboard.type(`${password}`);
+  await instance.keyboard.type(`${password}`);
 
-  // Submit Button //
-  const submitButton = await page.$('.btn-login');
+  const submitButton = await instance.$('.btn-login');
   await submitButton.click();
-  await page.waitFor(5000)
-  if (!await page.$('#headerlink-logout')) { // -> Verify if logout link is displayed
+  await instance.waitFor(5000)
+  if (!await instance.$('#headerlink-logout')) { // -> Verify if logout link is displayed
     console.log("\n- Can't Login -".red)
     process.exit()
-  } else {
+  } 
+  else {
     console.log('\n+ Logged In +\n'.green)
-    // Select World // 
-    const currentWorld = await page.$('.world_button_active'); // -> Select the first current world
+    const currentWorld = await instance.$('.world_button_active'); // -> Select the first current world
     await currentWorld.click();
-    await page.waitForNavigation();
+    await instance.waitForNavigation();
   }
-  // ---------------------------------------------------------------------- //
 
-  // Show Resources //
+  return instance;
 
-  const woodElement = await page.$('#wood'); // -> wood
-  const woodValue = await page.evaluate(el => el.textContent, woodElement) // -> wood
+}
 
-  const clayElement = await page.$('#stone'); // -> clay
-  const clayValue = await page.evaluate(el => el.textContent, clayElement) // -> clay
 
-  const ironElement = await page.$('#iron'); // -> iron
-  const ironValue = await page.evaluate(el => el.textContent, ironElement) // -> iron
+const showResources = async(instance) => {
 
-  const storageElement = await page.$('#storage'); // -> storage capacity
-  const storageValue = await page.evaluate(el => el.textContent, storageElement) // -> storage capacity
+  const woodElement = await instance.$('#wood'); // -> wood
+  const woodValue = await instance.evaluate(el => el.textContent, woodElement) // -> wood
 
-  const currFarmElement = await page.$('#pop_current_label'); // -> current farm
-  const currFarmValue = await page.evaluate(el => el.textContent, currFarmElement) // -> current farm
+  const clayElement = await instance.$('#stone'); // -> clay
+  const clayValue = await instance.evaluate(el => el.textContent, clayElement) // -> clay
 
-  const maxFarmElement = await page.$('#pop_max_label'); // -> max farm
-  const maxFarmValue = await page.evaluate(el => el.textContent, maxFarmElement) // -> max farm
+  const ironElement = await instance.$('#iron'); // -> iron
+  const ironValue = await instance.evaluate(el => el.textContent, ironElement) // -> iron
+
+  const storageElement = await instance.$('#storage'); // -> storage capacity
+  const storageValue = await instance.evaluate(el => el.textContent, storageElement) // -> storage capacity
+
+  const currFarmElement = await instance.$('#pop_current_label'); // -> current farm
+  const currFarmValue = await instance.evaluate(el => el.textContent, currFarmElement) // -> current farm
+
+  const maxFarmElement = await instance.$('#pop_max_label'); // -> max farm
+  const maxFarmValue = await instance.evaluate(el => el.textContent, maxFarmElement) // -> max farm
 
   const resourcesTable = new Table({
     chars: { 'top': '-' , 'top-mid': '+' , 'top-left': '+' , 'top-right': '+'
@@ -83,10 +98,32 @@ const main = async (username, password) => {
     [`${woodValue}`, `${clayValue}`, `${ironValue}`, `${storageValue}`, `${currFarmValue}/${maxFarmValue}` ]
   );
 
-  console.log(resourcesTable.toString());
+  console.log(resourcesTable.toString() + '\n');
 
-  // ---------------------------------------------------------------- //
-
+  return instance;
 }
 
-main(username, password)
+const gotoUpdatesPage = async(instance) => {
+  const headquarters = await instance.$('canvas.visual-anim:nth-child(31)');
+  await headquarters.click();
+  await instance.waitForNavigation();
+
+  return instance
+}
+
+
+const updateBuilding = async(instance, building) => {
+  console.log(`+ Trying to update ${building} +`.yellow)
+  //const buildingRow = await instance.$(`#main_buildrow_${building} > td:nth-child(7)`)  
+  //await buildingRow.click();
+}
+
+launchAndGetPage()
+  .then(page => login(page, username, password))
+  .then(page => showResources(page))
+  .then(page => gotoUpdatesPage(page))
+  .then(page => { while(true) {
+    updateBuilding(page, 'headquarters')
+    sleep(delayTime * 60000)
+  }
+  })
